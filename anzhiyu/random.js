79af2118fbcf5fdab033d5dbace7f2df772c3e8a
baiyun <2508237443@qq.com>
2024-10-15 15:39:24 +0800

    Site updated: 2024-10-15 15:39:24

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["undefined/d73d9aa0/","undefined/6c40434b/","undefined/e817ffda/","undefined/e5f66663/","undefined/91208508/","undefined/368776e6/","undefined/6b5f11fd/","undefined/e779e2ee/","undefined/8093261a/","undefined/3b7989e0/","undefined/2023caab/","undefined/a0a38a3f/","undefined/91208508/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
